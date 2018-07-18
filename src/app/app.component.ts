1a3033b38673baf54bb09f0729db94c80b30174f
Paul Cristerna <paul.cristerna@morsa.com.mx>
2018-07-18 17:05:48 -0600

    V1.0 - Angular CRUD frontend created.

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { GLOBAL } from './services/global';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title:string = 'Productos Angular 4';
+  public header_color: string;
+
+  constructor(){
+  	this.header_color = GLOBAL.header_color;
+  }
+
+}
